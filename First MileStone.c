91778bc4a825244f1dae4331ab20ce79cdf524a1
AliShawky003 <alishawky1562003@gmail.com>
2024-05-02 20:26:37 +0300

    Transmitting Function
    //Function to transfer data between the GPS module and the MC

--- First MileStone.c
+++ First MileStone.c
@@ -1,5 +1,13 @@
 #include "io.h"
 #include "tm4c123gh6pm.h"
+
+void UART0_transmit(char data) {			//Function to transfer data between the GPS module and the MC
+	while ((UART0_FR_R & 0x0020) != 0);
+	UART0_DR_R = data;
+}
+
+
+
 int main(void){
  GPIO_Init();
  SW_Init();
