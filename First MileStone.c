54ac7122c770d9c44ebedd64c3cbf114a3f5a303
Sherif-Alaa <sherif.faiizy2003@gmail.com>
2024-05-02 22:00:08 +0300

    Update First MileStone.c
    Switch Initialization

--- First MileStone.c
+++ First MileStone.c
@@ -5,6 +5,19 @@ void UART0_transmit(char data) {			//Function to transfer data between the GPS m
 	while ((UART0_FR_R & 0x0020) != 0);
 	UART0_DR_R = data;
 }
+//Switch Initialization
+void SW_Init(){
+
+GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+GPIO_PORTF_CR_R |= 0X10;
+GPIO_PORTF_AFSEL_R &= ~0X10;
+GPIO_PORTF_AMSEL_R &= ~0X10;
+GPIO_PORTF_DEN_R |= 0X10;
+GPIO_PORTF_PCTL_R &= ~0x000F0000;
+GPIO_PORTF_DIR_R &= ~0X10;
+GPIO_PORTF_PUR_R |= 0X10;
+}
+
 //LED  FUNCTION
 void RGB_Output(unsigned char data) {
  GPIO_PORTF_DATA_R &= ~PF123_mask;
