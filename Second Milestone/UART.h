53c37e9a59266b6a6c1d224684540f01b420fab1
FarahMoataz <168356157+FarahMoataz@users.noreply.github.com>
2024-05-16 23:31:39 +0300

    uart second part
    uart send and receive functions

--- Second Milestone/UART.h
+++ Second Milestone/UART.h
@@ -15,12 +15,12 @@ GPIO_PORTA_LOCK_R=GPIO_LOCK_KEY; //lock
 GPIO_PORTA_CR_R|=0x03;
 GPIO_PORTA_AFSEL_R|= 0x03; //enable alternate function
 GPIO_PORTA_PCTL_R=(GPIO_PORTA_PCTL_R & 0XFFFFFF00)+0X00000011; //enable UARTpins
-GPIO_PORTA_DEN_R|=0x03; 
-GPIO_PORTA_AMSEL_R &= ~(0x03); 
+GPIO_PORTA_DEN_R|=0x03;
+GPIO_PORTA_AMSEL_R &= ~(0x03);
 UART0_CTL_R &= ~0x0001; //Disable UART
 UART0_IBRD_R=104;  //int (16Mhz/16*9600)
 UART0_FBRD_R= 11;  // int ((16Mhz/16*9600 -104 )* 64+0.5)
-UART0_LCRH_R=0x0070; // enable FIFO 
+UART0_LCRH_R=0x0070; // enable FIFO
 UART0_CTL_R= 0x0301; // enable UART
             break;
         case UART2:
@@ -32,7 +32,7 @@ GPIO_PORTD_CR_R|=0xC0;
 GPIO_PORTD_AFSEL_R|= 0xC0; //enable alternate function
 GPIO_PORTD_PCTL_R=(GPIO_PORTD_PCTL_R & 0X00FFFFFF)+0X11000000; //enable UARTpins
 GPIO_PORTD_DEN_R|=0xC0;
-GPIO_PORTD_AMSEL_R &= ~0xC0; 
+GPIO_PORTD_AMSEL_R &= ~0xC0;
 UART2_CTL_R &= ~0x0001;  //Disable UART
 UART2_IBRD_R=104;  //int (16Mhz/16*9600)
 UART2_FBRD_R= 11; // int ((16Mhz/16*9600 -104 )* 64+0.5)
@@ -40,4 +40,42 @@ UART2_LCRH_R=0x0070; // enable FIFO
 UART2_CTL_R= 0x0301; // enable UART
             break;
     }
+}
+
+void UART_send(UARTNO uart_no, char data) {
+
+    // Select the appropriate UART data register based on uart_no
+    switch(uart_no) {
+        case UART0:
+            while((UART0_FR_R& 0x0020)!=0); //cheking if fifo is full
+               UART0_DR_R=data;
+
+            break;
+        case UART2:
+           while((UART2_FR_R& 0x0020)!=0); //cheking if fifo is full
+               UART2_DR_R=data;
+            break;
+    }
+}
+
+char UART_receive(UARTNO uart_no) {
+    // Select the appropriate UART data register based on uart_no
+    switch(uart_no) {
+        case UART0:
+while ((UART0_FR_R & 0x10)!=0) return 0;  //if PC fifo is empty return
+            return (char)(UART0_DR_R & 0xFF);
+            break;
+        case UART2:
+            while ((UART2_FR_R & 0x10)!=0) ;  //if GPS fifo is empty wait for data
+            return (char)(UART2_DR_R & 0xFF);
+            break;
+        default:
+            // Invalid UART number
+            return '\0';
+    }
+}
+void UART_OutPutStr(UARTNO uart_no,char ptr){
+while(ptr){
+UART_send(uart_no,*ptr); // send string charachter by charachter
+ptr++; }
 }
